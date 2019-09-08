58377a3588fb8b4dc295aafb5e3d99e72285c9bf
Nikita Gukov <isdeltas@gmail.com>
2019-09-08 14:48:19 +0300

    added redirect to movies from /

--- src/router.js
+++ src/router.js
@@ -9,6 +9,10 @@ export default new Router({
     base: process.env.BASE_URL,
     routes: [
         {
+            path: '/',
+            redirect: '/movies'
+        },
+        {
             path: '/movies',
             name: 'movies',
             component: Movies
